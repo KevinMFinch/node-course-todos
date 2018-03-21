bcd96cd848a6afa598fddab99446ce50117c72fd
Kevin <finchkevinm74@gmail.com>
2018-03-21 12:56:25 -0400

    GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,13 +9,20 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);  // Body stored by body parser
   var todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then((doc) => {
     res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
